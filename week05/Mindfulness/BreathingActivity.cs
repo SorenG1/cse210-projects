7f363a983f8914708ade27a750e8fc377fc52577
gomezs <gom23015@byui.edu>
2025-07-19 13:51:59 -0600

    Mindfulness Program

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,5 +1,6 @@
 using System;
 
+
 public class BreathingActivity : Activity
 {
     public BreathingActivity() : base("Breathing Activity", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.")
